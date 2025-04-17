2b85062e5dc80bf4244b97d9865055e6bc762083
Julia Andrade <99260860+JULIAPANDRADE@users.noreply.github.com>
2025-04-16 21:01:15 -0300

    Add files via upload

--- Atividade10/Atividade10.js
+++ Atividade10/Atividade10.js
@@ -1,1 +1,30 @@
+const peso = parseFloat(prompt("Insira o peso kg, ex: 80").replace(",", "."));
+const altura = parseFloat(prompt("Insira a altura em m, ex: 1,84 ").replace(",", "."));
 
+const calcImc = (peso, altura) => {
+  return peso / Math.pow(altura, 2);
+};
+
+const imc = calcImc(peso, altura);
+
+switch (true) {
+  case imc < 18.5:
+    alert("Magreza");
+    break;
+
+  case imc < 25:
+    alert("Normal");
+    break;
+
+  case imc < 30:
+    alert("Sobrepeso");
+    break;
+
+  case imc < 40:
+    alert("Obesidade");
+    break;
+
+  case imc > 40:
+    alert("Obesidade Grave");
+    break;
+}
