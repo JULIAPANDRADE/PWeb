6efdfd4b5a3b84767b3f440654e6619120897886
Julia Andrade <99260860+JULIAPANDRADE@users.noreply.github.com>
2025-04-26 22:12:53 -0300

    Add files via upload

--- Atividade11/Atividade11.js
+++ Atividade11/Atividade11.js
@@ -1,1 +1,26 @@
+let Funcionario01 = {};
+Funcionario01["matricula"] = "01";
+Funcionario01["nome"] = "Julia";
+Funcionario01["funcao"] = "Testadora de Software";
+
+
+
+let Funcionario02 = new Object();
+Funcionario02.matricula = "02";
+Funcionario02.nome = "Julia";
+Funcionario02.funcao = "Testadora de Software";
+
+
+
+let Funcionario03 = {};
+Funcionario03.matricula = "03";
+Funcionario03.nome = "Julia";
+Funcionario03.funcao = "Testadora de Software";
+
+
+alert("Objeto 1: " +JSON.stringify(Funcionario01));
+alert("Objeto 2: " +JSON.stringify(Funcionario02));
+alert("Objeto 3: " +JSON.stringify(Funcionario03));
+
+
 
