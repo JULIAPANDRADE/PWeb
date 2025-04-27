4b47ff265afa21f4dde63ac531fe773aef9249ec
Julia Andrade <99260860+JULIAPANDRADE@users.noreply.github.com>
2025-04-27 17:06:40 -0300

    Add files via upload

--- Atividade6/media.js
+++ Atividade6/media.js
@@ -1,1 +1,6 @@
+const name = prompt("Digite seu nome");
+const n1 = parseFloat(prompt("Digite a 1ª nota: "));
+const n2 = parseFloat(prompt("Digite a 2ª nota: "));
+const n3 = parseFloat(prompt("Digite a 3ª nota: "));
 
+alert(`média do aluno(a) ${name}: ${((n1 + n2 + n3) / 3).toFixed(2)}`);
